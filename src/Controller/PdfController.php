29a763f11d6ab84de46fb66191ed28032291a325
Edith <edith.bousquet@etu.isima.fr>
2020-01-15 16:34:43 +0100

    Ajout fichier

---
+++ src/Controller/PdfController.php
@@ -0,0 +1,23 @@
+<?php
+// src/Controller/Pdf.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Spipu\Html2Pdf\Html2Pdf;
+
+class PdfController
+{
+    public function generate()
+    {
+        var_dump(extension_loaded('curl'));
+        $html2pdf = new Html2Pdf('P', 'A4', 'fr');
+
+        /* A modifier selon le chemin */
+        /* !! chemins des images !! */
+        $content = file_get_contents('C:\Users\edith\Documents\projet\pdfgenerator\src\Controller\contenuDraft.html');
+        //$content = file_get_contents('C:\Users\edith\Documents\projet\pdfgenerator\src\Controller\contenuQuill.html');
+        
+        $html2pdf->writeHTML($content);
+        $html2pdf->output('example.pdf');
+    }
+}
