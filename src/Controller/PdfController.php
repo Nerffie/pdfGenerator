04407fd847bc4529245aeebd3c68d6635c5ad775
Edith <edith.bousquet@etu.isima.fr>
2020-01-30 15:49:10 +0100

    Parse Json

--- src/Controller/PdfController.php
+++ src/Controller/PdfController.php
@@ -4,20 +4,21 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Spipu\Html2Pdf\Html2Pdf;
+use nadar\quill\Lexer;
 
 class PdfController
 {
-    public function generate()
+    public function generatePdf()
     {
-        //var_dump(extension_loaded('curl'));
         $html2pdf = new Html2Pdf('P', 'A4', 'fr');
 
         /* A modifier selon le chemin */
         /* !! chemins des images !! */
-        //$content = file_get_contents('D:\Users\moham\Documents\Projet Isima\pdfgenerator\src\Controller\ContenuDraft.html');
-        $content = file_get_contents('D:\Users\moham\Documents\Projet Isima\pdfgenerator\src\Controller\ContenuQuill.html');
+        $content = file_get_contents('C:\Users\edith\Documents\projet\pdfgenerator\src\Controller\ContenuDraft.html');
+        //$content = file_get_contents('C:\Users\edith\Documents\projet\pdfgenerator\src\Controller\ContenuQuill.html');
         
         $html2pdf->writeHTML($content);
-        $html2pdf->output('resultatQuill.pdf');
+        $html2pdf->output('Exemple.pdf');
+        
     }
 }
